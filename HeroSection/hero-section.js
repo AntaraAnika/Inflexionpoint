1d02f71a53d42f985d9b3cf4bcbb92d41dc5564d
AntaraAnika <anika@insurecow.com>
2022-05-25 13:27:43 +0600

    InformationUpdate

--- HeroSection/hero-section.js
+++ HeroSection/hero-section.js
@@ -1,23 +1,23 @@
 // TYPING
 const texts = ['creativity'];
-let count = 0; 
+let count = 0;
 let index = 0;
 let currentText = '';
 let letter = '';
 
 
-(function type(){
-    if(count === texts.length){
-        count = 0;
-    }
-    currentText = texts[count];
-    letter = currentText.slice(0, ++index);
+(function type() {
+  if (count === texts.length) {
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
 
-    document.querySelector('.typing').textContent =letter;
-    if(letter.length === currentText.length){
-        count++;
-    }
-setTimeout(type, 400);
+  document.querySelector('.typing').textContent = letter;
+  if (letter.length === currentText.length) {
+    count++;
+  }
+  setTimeout(type, 400);
 
 }());
 
